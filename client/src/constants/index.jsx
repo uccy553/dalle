d8d9616a2d145065a6161265bd00f807a7041301
uche <77891127+uccy553@users.noreply.github.com>
2023-01-22 23:56:56 +0100

    Update index.jsx

--- client/src/constants/index.jsx
+++ client/src/constants/index.jsx
@@ -3,6 +3,7 @@ export const surpriseMePrompts = [
   'a surrealist dream-like oil painting by Salvador Dalí of a cat playing checkers',
   'teddy bears shopping for groceries in Japan, ukiyo-e',
   'an oil painting by Matisse of a humanoid robot playing chess',
+  'A herd of elephants crossing a savannah river at sunset',
   'panda mad scientist mixing sparkling chemicals, digital art',
   "a macro 35mm photograph of two mice in Hawaii, they're each wearing tiny swimsuits and are carrying tiny surf boards, digital art",
   '3D render of a cute tropical fish in an aquarium on a dark blue background, digital art',
